dd09d3427089321967890de40a082b43a9add522
Arshad008 <arshadrauff008@gmail.com>
2024-07-23 20:02:34 +0530

    modularizing predict script

--- gem-classifier-ai/predict.py
+++ gem-classifier-ai/predict.py
@@ -13,19 +13,23 @@ img_w, img_h = 220, 220    # width and height of image
 
 from common import known_classes
 from image_helper import edge_and_cut, prepare_image
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import Sequential, load_model
 
-model = load_model(model_path)
-print('model loaded')
+def initModel():
+    return load_model(model_path)
 
-image = prepare_image(train_dir + 'Garnet Red\garnet red_2.jpg')
-image = edge_and_cut(image)
-pred_image = np.array([image])
+def predict_image(model: Sequential, img_path):
+    image = prepare_image(img_path)
+    image = edge_and_cut(image)
+    pred_image = np.array([image])
 
-# print(pred_image)
-predict_x = model.predict(pred_image)
-pred_class = np.argmax(predict_x,axis=1)[0]
+    # print(pred_image)
+    predict_x = model.predict(pred_image)
+    pred_class = np.argmax(predict_x,axis=1)[0]
 
-print(pred_class)
-print(known_classes[pred_class])
-print("reached end")
+    print(pred_class)
+    print(known_classes[pred_class])
+
+# TODO uncomment following lines to test prediction
+model = initModel()
+predict_image(model,train_dir + 'Garnet Red\garnet red_2.jpg')
