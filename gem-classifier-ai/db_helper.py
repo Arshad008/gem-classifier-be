24c5e35cb05403651ddd10ab942f622d60a88a97
Arshad008 <arshadrauff008@gmail.com>
2024-07-24 18:11:23 +0530

    db helper

--- gem-classifier-ai/db_helper.py
+++ gem-classifier-ai/db_helper.py
@@ -1,11 +1,49 @@
 from flask import Flask, jsonify
 from flask_mysqldb import MySQL
+from datetime import datetime
+import uuid
 
 def initDb(app: Flask):
     app.config['MYSQL_HOST'] = 'localhost'
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'password'
-    app.config['MYSQL_DB'] = 'database_name'
+    app.config['MYSQL_PASSWORD'] = 'Password12345@@@'
+    app.config['MYSQL_DB'] = 'gem_classifier_db'
     return MySQL(app)
 
+class JobRecord:
+    jobId: str
+    note: str
+    classifiedClass: str
+    imageUrl: str
+    createdAt: datetime
 
+    def __init__(self, jobId: int, note: str, classifiedClass: str, imageUrl: str, createdAt: datetime):
+        self.jobId = jobId
+        self.note = note
+        self.classifiedClass = classifiedClass
+        self.imageUrl = imageUrl
+        self.createdAt = createdAt
+
+
+def create_job_record(dbInstance: MySQL, jobId: str, note: str, classifiedClass: str, imageUrl: str)-> JobRecord:
+    record = JobRecord(jobId, note, classifiedClass, imageUrl, datetime.now())
+    cur = dbInstance.connection.cursor()
+    cur.execute('''INSERT INTO gem_classification_jobs (`jobId`, `note`, `classifiedClass`, `imageUrl`, `createdAt`) VALUES (%s,%s,%s,%s,%s)''',
+                (jobId, note, classifiedClass, imageUrl, record.createdAt.isoformat(),))
+    dbInstance.connection.commit()
+    cur.close()
+    return record
+
+class UserRecord:
+    userId: str
+    name: str
+    password: str
+    lastLogin: datetime
+    createdAt: datetime
+
+    def __init__(self, userId: str, name: str, password: str, lastLogin: datetime, createdAt: datetime):
+        self.userId = userId
+        self.name = name
+        self.password = password
+        self.lastLogin = lastLogin
+        self.createdAt = createdAt
