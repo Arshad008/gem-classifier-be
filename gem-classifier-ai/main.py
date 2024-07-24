387596c78e55219b046627cd9cd595de7c3bdd63
Arshad008 <arshadrauff008@gmail.com>
2024-07-24 18:12:00 +0530

    create job

--- gem-classifier-ai/main.py
+++ gem-classifier-ai/main.py
@@ -1,8 +1,9 @@
 import os
 from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
+import uuid
 
-from db_helper import initDb
+from db_helper import initDb,create_job_record
 from web_service_helper import initWebServices, upload_file
 from predict import predict_image, initModel
 
@@ -10,33 +11,38 @@ app = Flask(__name__)
 model = initModel()
 
 initWebServices(app)
-initDb(app)
+dbInstance = initDb(app)
 
 @app.route('/')
 def hello_world():
+    # create_job_record(dbInstance, "Test note", "Test class", "test record")
     return jsonify({"status": 1})
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict_endpoint():
-    # if request.method == 'GET':
-    #     return '''
-    #         <h1>Upload new File</h1>
-    #         <form method="post" enctype="multipart/form-data">
-    #         <input type="file" name="file">
-    #         <input type="submit">
-    #         </form>
-    #         '''
+    if request.method == 'GET':
+        return '''
+            <h1>Upload new File</h1>
+            <form method="post" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <input type="text" name="note">
+            <input type="submit">
+            </form>
+            '''
 
-    result = {"success": False, "msg": "", "predict": None}
+    result = {"success": False, "msg": "", "predict": None, "data": None }
     upload = upload_file(app)
+    note = request.form.get("note")
 
     if(upload.isUploaded != True):
         result['msg'] = upload.msg
         return jsonify(result)
     
     predict_result = predict_image(model, upload.get_uploaded_filename())
-    print("[predict] -> img" + upload.get_uploaded_filename())
-    print("[predict] -> result " + predict_result)
+    jobId = uuid.uuid4()
+    create_job_record(dbInstance, jobId, note, predict_result, upload.get_uploaded_filename())
+    # print("[predict] -> img" + upload.get_uploaded_filename())
+    # print("[predict] -> result " + predict_result)
     result['predict'] = predict_result
     # result['predict'] = predict_result
     return jsonify(result)
